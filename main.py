5ce55ca07c4c1acc72b9fc740e1038dc4b7f82d8
Rafael Leite <rafaeldataengineer@outlook.com>
2025-10-21 11:07:06 -0300

    Adcionando opçãod e expandir objetos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Finanças", page_icon="💰")
 
@@ -10,4 +11,20 @@ st.markdown("""
             - Planeje seu orçamento
             """)
 
-st.file_uploader(label="Carregar arquivo de despesas", type=["csv", "xlsx"])
+# Widget de upload de arquivo
+file_upload = st.file_uploader(label="Carregar arquivo de despesas", type=["csv", "xlsx"])
+
+# Verificar se um arquivo foi carregado
+if file_upload is not None:
+    # Processar o arquivo carregado
+    df = pd.read_csv(file_upload)
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y').dt.date
+
+    # Exibir o conteúdo do arquivo
+    exp1 = st.expander("Dados Brutos")    
+    columns_fmt = {"Valor": st.column_config.NumberColumn("Valor", format="R$ %f")}
+    exp1.dataframe(df, hide_index=True, column_config=columns_fmt)
+
+    # Tabela dinâmica por instituição
+    df_instituicao = df.pivot_table(index='Data', columns='Instituição', values='Valor')
+    st.dataframe(df_instituicao)
