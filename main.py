afd747c9c5eca1f84ea72ef043b49bd8ba5206c3
Rafael Leite <rafaeldataengineer@outlook.com>
2025-10-21 09:46:45 -0300

    Configurando sistema de upload

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.text("Hello, Streamlit!")
+st.set_page_config(page_title="Finanças", page_icon="💰")
+
+st.markdown("""
+            # Finanças
+            ### Aqui você pode gerenciar suas finanças pessoais.
+            - Adicione suas despesas
+            - Visualize seus gastos
+            - Planeje seu orçamento
+            """)
+
+st.file_uploader(label="Carregar arquivo de despesas", type=["csv", "xlsx"])
